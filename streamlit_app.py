078e0c678330b58327e303ab9db75ed5f70b0c80
mariam2508 <mariamhossam243@gmail.com>
2024-08-26 22:24:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,15 @@ import pickle
 
 st.title("🎈Diabetes")
 
-Glucose=st.number_input('Glucose' , min_value=0.0 , max_value=199.00,value=0.0)
-Pregnancies=st.number_input('Pregnancies' , min_value=0.0 , max_value=17.0,value=0.0)
-BMI=st.number_input('BMI' , min_value=0.0 , max_value=67.10,value=0.0)
+Glucose = st.number_input('Glucose', min_value=0.0, max_value=199.0, value=0.0)
+Pregnancies = st.number_input('Pregnancies', min_value=0.0, max_value=17.0, value=0.0)
+BMI = st.number_input('BMI', min_value=0.0, max_value=67.1, value=0.0)
 
 with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
-output = model.predict([[Glucose,Pregnancies,BMI]])
-st.write("the predict of Diabetes:",output[0])  
+
+output = model.predict([[Glucose, Pregnancies, BMI]])
+st.write("The predicted outcome for Diabetes:", output[0])
 
 
 
