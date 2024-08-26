3e15f40265afce5a3dd3639fef986634b114575c
mariam2508 <mariamhossam243@gmail.com>
2024-08-26 22:06:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,11 @@ import pickle
 
 st.title("🎈Diabetes")
 
+Glucose=st.number_input('Glucose' , min_value=0.0 , max_value=199.00,value=0.0)
+Pregnancies=st.number_input('Pregnancies' , min_value=0.0 , max_value=17.0,value=0.0)
+BMI=st.number_input('BMI' , min_value=0.0 , max_value=67.10,value=0.0)
+
+
+
+
 
